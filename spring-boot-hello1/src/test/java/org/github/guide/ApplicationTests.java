0752d0594f594ce0c72a86eafc95ba5b51f57f1e
zhangrx <zhangrx@communet.io>
2016-12-03 02:27:19 +0800

    spring-boot1.4.0  HelloWorld
    Signed-off-by: zhangrx <zhangrx@communet.io>

---
+++ spring-boot-hello1/src/test/java/org/github/guide/ApplicationTests.java
@@ -0,0 +1,19 @@
+package org.github.guide;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by R on 2016/12/3.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class ApplicationTests {
+
+    @Test
+    public void testGetName() {
+
+    }
+}
